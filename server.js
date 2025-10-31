d871759e9f35e8b6eeb1eff9d8d74a9067aed0ff
WebX <darianbayan2@gmail.com>
2025-10-31 11:11:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -316,3 +316,4 @@ function generateHostHTML(username){
 // Start
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("chicken")
